68e2ce671e03ed44ef8e9d314fd4f59914bf0dac
BlackOnyxs <robinag2018@gmail.com>
2023-11-09 16:42:31 -0500

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export const App = () => {
+
+  return (
+    <>
+      <h1>Hey</h1>
+    </>
+  )
+}
